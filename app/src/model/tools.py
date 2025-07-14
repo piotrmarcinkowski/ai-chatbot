89c9207d712d9204b6b5b7a693e97394bbdef3e2
Piotr Marcinkowski <piotrmarc@gmail.com>
2025-07-14 18:01:15 +0000

    Wiki and Arxiv search tools

--- app/src/model/tools.py
+++ app/src/model/tools.py
@@ -1,5 +1,8 @@
 from langchain_core.tools import tool
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun
 from datetime import datetime
+from pydantic import BaseModel, Field
 
 @tool
 def current_date():
@@ -18,6 +21,34 @@ def search_context_in_chat_history(input_query: str):
     print(f"[tool] search_context_in_chat_history: Searching chat history for context related to: {input_query}")
     return _chatbot.search_memory_for_context(input_query, top_k=5)
 
+
+class ArxivTopic(BaseModel):
+    topic: str = Field(description="The topic of the article to search on arxiv.")
+
+@tool (args_schema=ArxivTopic)
+def arxiv_search(topic: str) -> str:
+    """Returns the information about research papers from arxiv"""
+    arxiv_api=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=300)
+    arxiv_tool=ArxivQueryRun(api_wrapper=arxiv_api)
+    print("Tool: arxiv_search - query: ", topic)
+    result = arxiv_tool.invoke(topic)
+    print("Tool: arxiv_search - result: ", result)    
+
+
+class WikipediaTopic(BaseModel):
+    topic: str = Field(description="The wikipedia article topic to search")
+
+@tool(args_schema = WikipediaTopic)
+def wikipedia_search(topic: str) -> str:
+    """Returns the summary of wikipedia page of the passed topic"""
+    api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=300)
+    wiki_tool=WikipediaQueryRun(api_wrapper=api_wrapper)
+    print("Tool: get_wiki_data - query: ", topic)
+    result = wiki_tool.invoke(topic)
+    print("Tool: get_wiki_data - result: ", result)
+    return result
+
+
 _chatbot = None
 
 def init_tools(chatbot):
@@ -27,7 +58,9 @@ def init_tools(chatbot):
     print("Initializing tools...")
     global _chatbot
     _chatbot = chatbot
-    tools = [current_date, search_context_in_chat_history]
+    tools = [current_date, 
+             search_context_in_chat_history,
+             arxiv_search, wikipedia_search]
     
     print(f"Tools initialized: {tools}")
     return tools
