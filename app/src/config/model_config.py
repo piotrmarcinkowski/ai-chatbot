1f735fb4d538e40851a819571d0c0ff980c2b18a
Piotr Marcinkowski <piotrmarc@gmail.com>
2025-08-09 16:16:17 +0200

    Add assistant.json config

--- app/src/config/model_config.py
+++ app/src/config/model_config.py
@@ -1,22 +1,28 @@
 import json
 import os
 
-def get_config_json_path():
+def get_config_json_path(file_name: str):
     """Get the path to the config.json file."""
     # Get the directory of the current file
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # Construct the path to config.json
-    config_path = os.path.join(current_dir, "config.json")
+    config_path = os.path.join(current_dir, file_name)
     return config_path
 
-class ModelConfig:
-    def __init__(self, config_file=get_config_json_path()):
+class Config:
+    """
+    A class to load and manage configuration settings from a JSON file.
+    """
+    def __init__(self, config_file_name: str):
         """Initialize the Config class and load the configuration from a JSON file."""
+
+        config_file = get_config_json_path(config_file_name)
 
         with open(config_file, "r", encoding="utf-8") as file:
             self.config = json.load(file)
 
     def get(self, key, default=None):
         return self.config.get(key, default)
-
-model_config = ModelConfig()
+    
+model_config = Config('config.json')
+assistant_config = Config('assistant.json')
