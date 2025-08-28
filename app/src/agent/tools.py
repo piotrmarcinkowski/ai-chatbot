2945343e14c1d7c271f6a35d71a1660a9048e114
Piotr Marcinkowski <piotrmarc@gmail.com>
2025-08-28 20:13:44 +0200

    Add logger - remove prints

--- app/src/agent/tools.py
+++ app/src/agent/tools.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel, Field
 from utils.time import get_current_local_time, get_current_time, get_local_time_zone
 from langchain_google_community import GoogleSearchAPIWrapper
 import os
+import logging
+
+log = logging.getLogger(__name__)
 
 @tool
 def current_utc_time():
@@ -39,9 +42,9 @@ def arxiv_search(topic: str) -> str:
     """Returns the information about research papers from arxiv"""
     arxiv_api=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=300)
     arxiv_tool=ArxivQueryRun(api_wrapper=arxiv_api)
-    print("Tool: arxiv_search - query: ", topic)
+    log.debug("Tool: arxiv_search - query: %s", topic)
     result = arxiv_tool.invoke(topic)
-    print("Tool: arxiv_search - result: ", result)
+    log.debug("Tool: arxiv_search - result: %s", result)
     return result
 
 
@@ -53,9 +56,9 @@ def wikipedia_search(topic: str) -> str:
     """Returns the summary of wikipedia page of the passed topic"""
     api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=300)
     wiki_tool=WikipediaQueryRun(api_wrapper=api_wrapper)
-    print("Tool: get_wiki_data - query: ", topic)
+    log.debug("Tool: get_wiki_data - query: %s", topic)
     result = wiki_tool.invoke(topic)
-    print("Tool: get_wiki_data - result: ", result)
+    log.debug("Tool: get_wiki_data - result: %s", result)
     return result
 
 @tool
@@ -69,14 +72,14 @@ def google_web_search(query: str) -> str:
         google_cse_id=os.getenv("GOOGLE_CSE_ID")
     )
     results = google_search.results(query=query, num_results=5)
-    print(f"Google search results for query '{query}': {results}")
+    log.debug("Google search results for query '%s': %s", query, results)
     return results
 
 def init_tools():
     """
     Initializes and returns a list of tools.
     """
-    print("Initializing tools...")
+    log.info("Initializing tools...")
     _tools = [current_utc_time, current_local_time, local_time_zone,
              arxiv_search, wikipedia_search
     ]
@@ -85,12 +88,12 @@ def init_tools():
     _google_api_key = os.getenv("GOOGLE_API_KEY")
     _google_cse_id = os.getenv("GOOGLE_CSE_ID")
     if _google_api_key and _google_cse_id:
-        print("Google API key and CSE ID found - activating Google search tool.")
+        log.info("Google API key and CSE ID found - activating Google search tool.")
         _tools.append(google_web_search)
     else:
-        print("Google API key and CSE ID must be set in environment variables. Google search tool will not be available.")
+        log.warning("Google API key and CSE ID must be set in environment variables. Google search tool will not be available.")
     
-    print(f"Tools initialized: {_tools}")
+    log.info(f"Tools initialized: {_tools}")
     return _tools
 
 tools = init_tools()
